c791dc973529fa47df7dcd96cdeb161fdd9f125f
pvorona <vorona.pavel.nickolaevich@gmail.com>
2026-03-04 00:58:48 +0400

    Stub `createReference` with `Reference` and `ReadonlyReference` types

--- packages/reference/src/lib/reference.ts
+++ packages/reference/src/lib/reference.ts
@@ -1,3 +1,17 @@
-export function reference(): string {
-  return 'reference';
+export type ReadonlyReference<T> = Readonly<{
+  getOr: <U>(valueOrGetter: U | (() => U)) => T | U;
+  getOrThrow: (messageOrFactory?: string | (() => string)) => T;
+}>;
+
+export type Reference<T> = Readonly<{
+  getOr: <U>(valueOrGetter: U | (() => U)) => T | U;
+  getOrThrow: (messageOrFactory?: string | (() => string)) => T;
+  getOrSet: (valueOrGetter: T | (() => T)) => T;
+  set: (value: T) => void;
+  unset: () => void;
+  asReadonly: () => ReadonlyReference<T>;
+}>;
+
+export function createReference<T>(initialValue: T): Reference<T> {
+  throw new Error('Not implemented');
 }
