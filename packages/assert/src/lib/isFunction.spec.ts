8de2d2b6acc24a06275ccfc555e6f7cd7295aaf7
pvorona <vorona.pavel.nickolaevich@gmail.com>
2026-03-07 16:13:10 +0400

    Fix `isFunction` narrowing for `unknown`

--- packages/assert/src/lib/isFunction.spec.ts
+++ packages/assert/src/lib/isFunction.spec.ts
@@ -1,12 +1,16 @@
 import { isFunction } from './isFunction.js';
 
+type AnyFunction =
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  Function;
+
 test('isFunction', () => {
   expect(isFunction(() => 'value')).toBe(true);
   expect(isFunction('value')).toBe(false);
 
   const unknownValue = (() => 'value') as unknown;
   if (isFunction(unknownValue)) {
-    expectTypeOf(unknownValue).toEqualTypeOf<never>();
+    expectTypeOf(unknownValue).toEqualTypeOf<AnyFunction>();
   }
 
   const maybeFunction = (() => 'value') as string | (() => string);
@@ -15,5 +19,13 @@ test('isFunction', () => {
     expect(maybeFunction()).toBe('value');
   } else {
     expectTypeOf(maybeFunction).toEqualTypeOf<string>();
+  }
+
+  const maybeErrorOrFunction = (() => 'value') as Error | (() => string);
+  if (isFunction(maybeErrorOrFunction)) {
+    expectTypeOf(maybeErrorOrFunction).toEqualTypeOf<() => string>();
+    expect(maybeErrorOrFunction()).toBe('value');
+  } else {
+    expectTypeOf(maybeErrorOrFunction).toEqualTypeOf<Error>();
   }
 });
--- packages/assert/src/lib/isFunction.ts
+++ packages/assert/src/lib/isFunction.ts
@@ -2,6 +2,15 @@ type AnyFunction =
   // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
   Function;
 
-export function isFunction<T>(value: T): value is Extract<T, AnyFunction> {
+type IsUnknown<T> = unknown extends T
+  ? [keyof T] extends [never]
+    ? true
+    : false
+  : false;
+
+type NarrowFunction<T> =
+  IsUnknown<T> extends true ? T & AnyFunction : Extract<T, AnyFunction>;
+
+export function isFunction<T>(value: T): value is NarrowFunction<T> {
   return typeof value === 'function';
 }
